164ba12aab2bd51000c6dabdf73cf0234195ebf9
mqxu <moqi1977@gmail.com>
2021-04-20 10:39:45 +0800

    rem移动端适配

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <van-button type="primary">默认按钮</van-button>
+    <div>你好</div>
   </div>
 </template>
 
@@ -9,3 +9,13 @@ export default {
   name: "Home",
 };
 </script>
+<style lang="less" scoped>
+.home {
+  div {
+    background: grey;
+    width: 150px;
+    height: 150px;
+    font-size: 18px;
+  }
+}
+</style>
