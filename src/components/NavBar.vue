1c969084ea8cd887715bdcfc3988e948336e284d
mqxu <moqi1977@gmail.com>
2021-04-22 13:54:48 +0800

    接口请求封装

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,19 +2,19 @@
   <div class="nav-bar">
     <ul class="nav-list">
       <router-link tag="li" class="nav-list-item active" to="home">
-        <i class="nbicon nblvsefenkaicankaoxianban-1"></i>
+        <i class="iconfont icon-home"></i>
         <span>首页</span>
       </router-link>
       <router-link tag="li" class="nav-list-item" to="category">
-        <i class="nbicon nbfenlei"></i>
+        <i class="iconfont icon-category"></i>
         <span>分类</span>
       </router-link>
       <router-link tag="li" class="nav-list-item" to="cart">
-        <i class="nbicon nbgouwuche"></i>
+        <i class="iconfont icon-cart"></i>
         <span>购物车</span>
       </router-link>
       <router-link tag="li" class="nav-list-item" to="user">
-        <i class="nbicon nblvsefenkaicankaoxianban-"></i>
+        <i class="iconfont icon-user"></i>
         <span>我的</span>
       </router-link>
     </ul>
