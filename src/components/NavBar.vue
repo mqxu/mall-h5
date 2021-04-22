1c95fa05bfcdf5bbdda8ad95e387fb93bb48fb9d
mqxu <moqi1977@gmail.com>
2021-04-22 11:20:22 +0800

    添加iconfont图标

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,15 +2,19 @@
   <div class="nav-bar">
     <ul class="nav-list">
       <router-link tag="li" class="nav-list-item active" to="home">
+        <i class="nbicon nblvsefenkaicankaoxianban-1"></i>
         <span>首页</span>
       </router-link>
       <router-link tag="li" class="nav-list-item" to="category">
+        <i class="nbicon nbfenlei"></i>
         <span>分类</span>
       </router-link>
       <router-link tag="li" class="nav-list-item" to="cart">
+        <i class="nbicon nbgouwuche"></i>
         <span>购物车</span>
       </router-link>
       <router-link tag="li" class="nav-list-item" to="user">
+        <i class="nbicon nblvsefenkaicankaoxianban-"></i>
         <span>我的</span>
       </router-link>
     </ul>
