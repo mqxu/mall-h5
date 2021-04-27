1e2d4530b9693fab727463fbf20a683a89e4a227
mqxu <moqi1977@gmail.com>
2021-04-27 09:50:58 +0800

    上传文件名处理

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,9 +1,14 @@
 import uuidv1 from "uuid/v1"
+
+/**
+ * 文件名重命名
+ * @param {*} fileName 
+ * @returns 
+ */
 export function renameFile (fileName) {
-    let originalName = fileName
-    const index = originalName.lastIndexOf(".")
-    const suffixName = originalName.substr(index)
+    const originalName = fileName   //原文件名
+    const suffixName = originalName.substr(originalName.lastIndexOf(".")) //扩展名，如 .jpg
     console.log(suffixName)
-    let targetName = uuidv1() + originalName.substr(index)
+    let targetName = uuidv1() + suffixName //uuid+扩展名=>新文件名
     return targetName
 }
